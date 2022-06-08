62df6ef27dc2c2e27afbd29683c78d322aaf46a9
kifuan <kifuan@foxmail.com>
2022-06-08 18:18:48 +0800

    refactor: move options inside the class

--- src/collection.ts
+++ src/collection.ts
@@ -23,7 +23,8 @@ export type CollectionOptions<T> = {
  * it will start a so-called "debounced" function to save the data.
  */
 export class Collection<T> {
-    private readonly options: CollectionOptions<T>
+    private readonly elements: T[]
+    private readonly save: () => void
 
     /**
      * YOU SHOULD NOT CREATE COLLECTION BY YOURSELF.
@@ -31,11 +32,8 @@ export class Collection<T> {
      * @param options the options of this collection
      */
     constructor(options: CollectionOptions<T>) {
-        this.options = options
-    }
-
-    private save() : void {
-        this.options.save.apply(undefined)
+        this.elements = options.elements
+        this.save = options.save
     }
 
     /**
@@ -43,8 +41,7 @@ export class Collection<T> {
      * @param data the data to be inserted
      */
     insert(...data: T[]) : void {
-        const { elements } = this.options
-        elements.push(...data)
+        this.elements.push(...data)
         this.save()
     }
 
@@ -55,21 +52,20 @@ export class Collection<T> {
      * @return whether the data is updated
      */
     update(data: Partial<T> | T, cond: Condition<T>) : boolean {
-        const { elements } = this.options
-        const index = elements.findIndex(cond)
+        const index = this.elements.findIndex(cond)
 
         if (index === -1) {
             return false
         }
 
-        const found = elements[index]
+        const found = this.elements[index]
 
         if (found instanceof Object) {
             Object.assign(found, data)
         } else {
             // Primitive types have no properties,
             // so it must be the type itself.
-            elements[index] = data as T
+            this.elements[index] = data as T
         }
 
         this.save()
@@ -82,12 +78,12 @@ export class Collection<T> {
      * @return whether the data is deleted
      */
     delete(cond: Condition<T>) : boolean {
-        const { elements } = this.options
-        const index = elements.findIndex(cond)
+        const index = this.elements.findIndex(cond)
         if (index === -1) {
             return false
         }
-        elements.splice(index, 1)
+
+        this.elements.splice(index, 1)
         this.save()
         return true
     }
@@ -98,15 +94,13 @@ export class Collection<T> {
      * @returns the data that matches given condition, or undefined if there is not
      */
     find(cond: Condition<T>) : T | undefined {
-        const { elements } = this.options
-        return elements.find(cond)
+        return this.elements.find(cond)
     }
 
     /**
      * The length of this collection.
      */
     get length() {
-        const { elements } = this.options
-        return elements.length
+        return this.elements.length
     }
 }
